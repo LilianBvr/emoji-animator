ef223eda954d8437964a1f10a644fb348bb608b4
Lilian BOUVIER <lilian.bouvier@etu.unistra.fr>
2021-05-26 15:53:09 +0200

    Get someting that works

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ function main() {
   var btn = document.querySelector(".trigger-animation");
   const params = {
     container: document.querySelector(".screen-container"),
-    duration: 1000,
-    qty: 30,
+    duration: 3000,
+    qty: 20
   }
   btn.addEventListener("click", () => triggerAnimation(params));
 }
@@ -20,5 +20,21 @@ function main() {
  *  }
  */
 function triggerAnimation(params) {
-  
+  for(var i=0; i< params.qty; i++) {
+    var elem = document.createElement("div");
+    elem.className = "animated-emoji";
+    elem.innerHTML = "🥳";
+    elem.style.left = Math.random()*100+"%";
+    elem.style.animation = "move "+params.duration+"ms"
+    showEmoji(elem, params.container, Math.random()*params.duration/2);
+  }
+  setTimeout(removeEmojis, params.duration*1.5);
+}
+
+function showEmoji(elem, container, delay) {
+  setTimeout(function(){container.append(elem)}, delay);
+}
+
+function removeEmojis() {
+  document.querySelectorAll(".animated-emoji").forEach(e=>e.remove())
 }
