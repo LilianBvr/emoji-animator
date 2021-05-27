f5052c3ac1d6a436445ebe50fcfe134077a61209
Lilian BOUVIER <lilian.bouvier@etu.unistra.fr>
2021-05-27 09:26:51 +0200

    Removed collection

--- js/emoji-animator.js
+++ js/emoji-animator.js
@@ -3,27 +3,19 @@ class EmojiAnimator {
     this.duration = duration;
     this.qty = qty;
     this.container = container;
-    this.collection = [];
   }
 
   //Creates a set of passed emoji and animate them
   animate(emoji) {
-    this.createSet(emoji);
-    for(var i=0; i<this.qty; i++) {
-      var delay = Math.random()*this.duration/2;
-      this.showEmoji(this.collection[i], delay);
-      this.removeEmoji(this.collection[i], delay);
-    }
-  }
-
-  createSet(emoji) {
     for(var i=0; i< this.qty; i++) {
       var elem = document.createElement("div");
       elem.className = "animated-emoji";
       elem.innerHTML = emoji;
       elem.style.left = Math.random()*100+"%";
       elem.style.animation = "wave "+this.duration+"ms ease-in-out"
-      this.collection.push(elem);
+      var delay = Math.random()*this.duration/2;
+      this.showEmoji(elem, delay);
+      this.removeEmoji(elem, delay);
     }
   }
 
@@ -32,8 +24,9 @@ class EmojiAnimator {
   }
 
   removeEmoji(elem, delay) {
-    setTimeout(()=>{ if(elem) elem.remove(); }, delay+this.duration);
-    this.collection.shift();
+    setTimeout(()=>{
+      elem.remove();
+    }, delay+this.duration);
   }
 
 }
